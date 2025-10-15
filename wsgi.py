f3751cdb2211dfb4fea6564185d61bf9f9d0a0ec
dat.ngo <dat.ngo@innogram.co>
2025-10-15 16:28:32 +0700

    Refactor WSGI configuration to streamline sys.path management and clean up commented code

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,20 @@ from src.track_py.const import PROJECT_HOME
 from src.track_py.main import app
 
 # Add your project directory to the Python path
-if PROJECT_HOME not in sys.path:
-    sys.path = [PROJECT_HOME] + sys.path
+# if PROJECT_HOME not in sys.path:
+#     sys.path = [PROJECT_HOME] + sys.path
 
-# For PythonAnywhere, the WSGI application should be called 'application'
+# # For PythonAnywhere, the WSGI application should be called 'application'
+# application = app
+
+# Add src folder to sys.path
+if PROJECT_HOME not in sys.path:
+    sys.path.insert(0, PROJECT_HOME)
+
+# Import app from the package inside src (no 'src.' prefix)
+from track_py.main import app
+
+# Required for PythonAnywhere
 application = app
 
 # For local testing purposes
